5a703aac767f028f17550e6ebd9e50c710146ffb
Umar <umar@busanid.dev>
2023-05-03 15:47:30 +0700

    add mdi icon

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Header: React.FC = () => {
     return (
-        <header className="header fixed flex right-0 left-20 bg-red-100">
-            <div className="inner container m-auto bg-green-100">inner</div>
+        <header className="header fixed flex right-0 left-20">
+            <div className="inner container m-auto"></div>
         </header>
     )
 }
