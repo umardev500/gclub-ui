223dc17c32ac0d8c24efa55324df7fb7036f3faf
Umar <umar@busanid.dev>
2023-05-03 15:26:33 +0700

    initial profile menu

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export * from './sidebar'
 export * from './sidebarMenuList'
 export * from './header'
+export * from './profileMenu'
---
+++ src/components/organisms/profileMenu/ProfileMenu.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ProfileMenu: React.FC = () => {
+    return <div></div>
+}
