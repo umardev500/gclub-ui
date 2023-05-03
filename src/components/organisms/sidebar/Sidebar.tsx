bdb02bf3f85c628440066d917f72664e69fb4485
Umar <umar@busanid.dev>
2023-05-03 14:30:55 +0700

    make sidebar fixed

--- src/components/organisms/sidebar/Sidebar.tsx
+++ src/components/organisms/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarMenuList } from '..'
 
 export const Sidebar: React.FC = () => {
     return (
-        <aside className="sidebar flex items-start justify-center bg-white absolute top-0 left-0 bottom-0 w-20">
+        <aside className="sidebar flex items-start justify-center bg-white fixed top-0 left-0 bottom-0 w-20">
             <div className="inner flex flex-col items-center py-2">
                 <div className="flex mb-4 px-4 py-2 items-center m-auto cursor-pointer">
                     <img className="w-8" src="assets/logos/logo.svg" alt="" />
