1a5194b8c89c7f8f48aaebb1c304eb8affff4d69
Umar <umar@busanid.dev>
2023-05-03 13:55:48 +0700

    add sidebar menu list

--- src/components/organisms/sidebar/Sidebar.tsx
+++ src/components/organisms/sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { SidebarMenuList } from '..'
 
 export const Sidebar: React.FC = () => {
     return (
-        <aside>
-            sidebar
-            <div style={{ width: 50, height: 50 }} className="bg-red-300 px-4">
-                <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10,3H4A1,1,0,0,0,3,4V8A1,1,0,0,0,4,9h6a1,1,0,0,0,1-1V4A1,1,0,0,0,10,3ZM9,7H5V5H9Zm1,4H4a1,1,0,0,0-1,1v8a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V12A1,1,0,0,0,10,11ZM9,19H5V13H9Zm11-4H14a1,1,0,0,0-1,1v4a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V16A1,1,0,0,0,20,15Zm-1,4H15V17h4ZM20,3H14a1,1,0,0,0-1,1v8a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V4A1,1,0,0,0,20,3Zm-1,8H15V5h4Z"></path>
-                </svg>
+        <aside className="sidebar flex items-start justify-center bg-white absolute top-0 left-0 bottom-0 w-20">
+            <div className="inner flex flex-col items-center py-2">
+                <div className="flex mb-4 px-4 py-2 items-center m-auto cursor-pointer">
+                    <img className="w-8" src="assets/logos/logo.svg" alt="" />
+                </div>
+
+                <SidebarMenuList />
             </div>
         </aside>
     )
