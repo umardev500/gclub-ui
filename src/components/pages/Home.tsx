30a07b2f57da702f2a713a6c1b6465ad59b725a1
Umar <umar@busanid.dev>
2023-05-03 11:32:40 +0700

    make home view

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Sidebar } from '..'
+
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { router } from '@router/Router.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
