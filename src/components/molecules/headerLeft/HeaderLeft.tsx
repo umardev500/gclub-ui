eb501de7503d2d46772a18b7ec975e9609aaa14f
Umar <umar@busanid.dev>
2023-05-03 16:02:14 +0700

    add initial header left

---
+++ src/components/molecules/headerLeft/HeaderLeft.tsx
@@ -0,0 +1,11 @@
+import { SidebarTrigger } from '@components/atoms'
+import React from 'react'
+
+export const HeaderLeft: React.FC = () => {
+    return (
+        <div className="flex items-center gap-4">
+            <SidebarTrigger />
+            <h3 className="text-xl font-medium text-gray-600">Home</h3>
+        </div>
+    )
+}
--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,9 +1,12 @@
+import { HeaderLeft } from '@components/molecules'
 import React from 'react'
 
 export const Header: React.FC = () => {
     return (
         <header className="header fixed flex right-0 left-20">
-            <div className="inner container m-auto"></div>
+            <div className="inner container m-auto">
+                <HeaderLeft />
+            </div>
         </header>
     )
 }
