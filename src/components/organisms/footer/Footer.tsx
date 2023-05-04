40948a2e7cb437979f94c714891aa2346f92f4b0
Umar <umar@busanid.dev>
2023-05-04 17:03:53 +0700

    add footer

---
+++ src/components/organisms/footer/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Footer: React.FC = () => {
+    return <div className="container text-gray-500 flex gap-2 items-center justify-center mx-auto h-20">&copy; 2023 Built with React Made with ❤️</div>
+}
