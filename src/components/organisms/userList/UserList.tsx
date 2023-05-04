e242d03d35f04b7429910d84ff5864f954837b73
Umar <umar@busanid.dev>
2023-05-04 12:20:41 +0700

    add dummy list

--- src/components/organisms/userList/UserList.tsx
+++ src/components/organisms/userList/UserList.tsx
@@ -1,5 +1,52 @@
-import React from 'react'
+import { mdiDelete, mdiUpdate } from '@mdi/js'
+import Icon from '@mdi/react'
+import { getCoreRowModel, useReactTable, type ColumnDef, flexRender } from '@tanstack/react-table'
 
-export const UserList: React.FC = () => {
-    return <div></div>
+interface Props<T extends object> {
+    data: T[]
+    columns: Array<ColumnDef<T>>
+}
+
+export const UserList = <T extends object>({ data, columns }: Props<T>) => {
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
+    return (
+        <div className="mt-8 mb-8">
+            <table className="table w-full text-left text-gray-600">
+                <thead className="text-gray-600 uppercase bg-gray-50 text-sm ubuntu">
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th className="font-medium px-6 py-3" key={header.id}>
+                                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                </th>
+                            ))}
+                            <th className="font-medium px-6 py-3 text-center">Action</th>
+                        </tr>
+                    ))}
+                </thead>
+
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id} className="border-b border-b-gray-100">
+                            {row.getVisibleCells().map((cell) => (
+                                <td className="px-6 py-4" key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                            <td className="px-6 py-4text-center">
+                                <div className="flex gap-1 justify-center">
+                                    <button className="w-8 h-8 flex items-center justify-center text-white rounded-md bg-red-600 hover:bg-red-700">
+                                        <Icon path={mdiDelete} size={'22px'} />
+                                    </button>
+                                    <button className="w-8 h-8 flex items-center justify-center text-white rounded-md bg-sky-600 hover:bg-sky-700">
+                                        <Icon path={mdiUpdate} size={'22px'} />
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
--- src/components/pages/user/User.tsx
+++ src/components/pages/user/User.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import { UserList } from '@components/organisms'
+import { type ColumnDef } from '@tanstack/react-table'
+import React, { useMemo } from 'react'
+
+interface Item {
+    id?: number
+    username: string
+    name: string
+    role: string
+    status: string
+}
+
+const dummyData = () => {
+    const items: Item[] = []
+    for (let i = 0; i < 10; i++) {
+        items.push({
+            id: i,
+            username: `username ${i}`,
+            name: `Item ${i}`,
+            role: `role ${i}`,
+            status: `status ${i}`,
+        })
+    }
+    return items
+}
 
 export const User: React.FC = () => {
+    const cols = useMemo<Array<ColumnDef<Item>>>(() => {
+        return [
+            {
+                header: 'Username',
+                cell: (row) => row.renderValue(),
+                accessorKey: 'username',
+            },
+            {
+                header: 'Name',
+                cell: (row) => row.renderValue(),
+                accessorKey: 'name',
+            },
+            {
+                header: 'Role',
+                cell: (row) => row.renderValue(),
+                accessorKey: 'role',
+            },
+            {
+                header: 'Status',
+                cell: (row) => row.renderValue(),
+                accessorKey: 'status',
+            },
+        ]
+    }, [])
+
     return (
-        <div className="container flex h-14 m-auto">
+        <div className="container m-auto bg-white rounded-md border py-6 px-8">
             <div>
                 <h3 className="text-gray-600 text-xl font-semibold">Account List</h3>
+            </div>
+            <div>
+                <UserList data={dummyData()} columns={cols} />
             </div>
         </div>
     )
