928e3a45c4baedc3660a11eba444c72b5b08528b
Umar <umar@busanid.dev>
2023-05-04 08:52:11 +0700

    use toggle hooks in trigger button of sidebar

--- src/components/atoms/sidebarTrigger/SidebarTrigger.tsx
+++ src/components/atoms/sidebarTrigger/SidebarTrigger.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useToggle } from '@hooks/useToggle'
+import React, { useRef } from 'react'
 
 export const SidebarTrigger: React.FC = () => {
+    const btnRef = useRef<HTMLButtonElement>(null)
+    useToggle(btnRef)
+
     return (
-        <button className="text-sky-600 flex items-center justify-center">
+        <button ref={btnRef} className="text-sky-600 flex items-center justify-center">
             <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H5C4.44772 7 4 6.55228 4 6Z" fill="currentColor" />
                 <path d="M4 18C4 17.4477 4.44772 17 5 17H19C19.5523 17 20 17.4477 20 18C20 18.5523 19.5523 19 19 19H5C4.44772 19 4 18.5523 4 18Z" fill="currentColor" />
