ace2b1413224604a58d22a6f17e46726317c5c03
Umar <umar@busanid.dev>
2023-05-04 10:15:44 +0700

    remove unused import

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Header, ProfileMenu, Sidebar } from '..'
+import { Header, Sidebar } from '..'
 
 export const Home: React.FC = () => {
     return (
         <div id="root-view" className="shown">
             <Header />
             <Sidebar />
-            {/* <ProfileMenu /> */}
         </div>
     )
 }
