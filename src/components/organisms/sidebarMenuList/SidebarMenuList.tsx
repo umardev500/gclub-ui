479071a2c1edc42739429b9ebe1de9fbed4657a3
Umar <umar@busanid.dev>
2023-05-04 08:59:18 +0700

    comment logout sidebar menu

--- src/components/organisms/sidebarMenuList/SidebarMenuList.tsx
+++ src/components/organisms/sidebarMenuList/SidebarMenuList.tsx
@@ -41,11 +41,11 @@ export const SidebarMenuList: React.FC = () => {
                     <path d="M12 8V12L15 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
             </a>
-            <a href="#" className="text-gray-400 px-4 outline-none">
+            {/* <a href="#" className="text-gray-400 px-4 outline-none">
                 <svg className="w-6 h-6" fill="currentColor" focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="LoginIcon">
                     <path d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"></path>
                 </svg>
-            </a>
+            </a> */}
         </div>
     )
 }
