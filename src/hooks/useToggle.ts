6747b1d52e49dbf86e4faae4b21ff22ddcbbcfe5
Umar <umar@busanid.dev>
2023-05-04 08:51:02 +0700

    change HTMLDivElement to HtmlELement to make it globaly

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,7 @@
 import type React from 'react'
 import { useCallback, useEffect } from 'react'
 
-export const useToggle = (ref: React.RefObject<HTMLDivElement>) => {
+export const useToggle = (ref: React.RefObject<HTMLElement>) => {
     const clickHandler = useCallback(() => {
         const rootView = document.querySelector('#root-view')
         const isOpen = rootView?.classList.contains('shown') ?? false
