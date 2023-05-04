d628e2468c248063ee33f9b3b6541e8141f76106
Umar <umar@busanid.dev>
2023-05-04 17:17:37 +0700

    default to hide sidebar

--- src/router/RootView.tsx
+++ src/router/RootView.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export const RootView: React.FC = () => {
     return (
-        <div id="root-view" className="shown">
+        <div id="root-view" className="">
             <Header />
             <Sidebar />
             <div className="inner-view px-4 flex flex-col justify-between min-h-screen">
